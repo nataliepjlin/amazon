12cceb346dc5dd556ac22f9da41fb43302f461bd
nat <nataliepjlin@gmail.com>
2024-08-08 18:07:38 +0800

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,6 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  price: 1090,//cent
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  price: 2095,//cent
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  price: 799,//cent
-}, ];
-
 let productsHTML = '';
 products.forEach((product) => {
-  const {image, name, rating, price} = product;
+  const {id, image, name, rating, price} = product;
   productsHTML += `<div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
