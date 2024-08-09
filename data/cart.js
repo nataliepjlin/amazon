511c87a25ca22de4a9760efcb82a6de2d8c9e8d6
nat <nataliepjlin@gmail.com>
2024-08-09 15:27:52 +0800

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,14 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
   id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }];
+
+export function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addItem(id){
   let matchingItem, cartQuantity = 0;
@@ -25,8 +29,10 @@ export function addItem(id){
 
   cartQuantity += choice;
   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  saveToStorage();
   console.log(cart);
 }
 export function removeItem(id){
   cart = cart.filter((cartItem) => cartItem.id !== id);
+  saveToStorage();
 }
